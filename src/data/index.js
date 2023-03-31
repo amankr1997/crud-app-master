f842e7fe1ac890b9c279cd3c4f49ffffe916ee5b
amankr1997 <128629274+amankr1997@users.noreply.github.com>
2023-03-31 17:12:07 +0530

    Initial commit

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Aman',
+    lastName: 'Kumar',
+    email: 'aman@example.com',
+    salary: '95000',
+    date: '2023-04-11',
+  },
+  {
+    id: 2,
+    firstName: 'Ashish',
+    lastName: 'kumar',
+    email: 'ashish@example.com',
+    salary: '80000',
+    date: '2023-04-17',
+  },
+  {
+    id: 3,
+    firstName: 'HR',
+    lastName: 'Tender247',
+    email: 'hr@example.com',
+    salary: '79000',
+    date: '2023-05-01',
+  },
+  
+];
+
+export { employeesData };
